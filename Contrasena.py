54f454b93dd8a1caf570f33e59529a692630238e
Teo716 <benjamore1212@gmail.com>
2025-03-19 21:47:13 -0300

    Contrasena.py

---
+++ Contrasena.py
@@ -0,0 +1,11 @@
+import random
+caracteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+longitud = int(input("¿De que longitud queres la contraseña?"))
+contrasena = "H"
+for i in range(longitud):
+    numero = random.randint(1,75)
+    if i == 0:
+        contrasena = caracteres[numero]
+    else:
+        contrasena += caracteres[numero]
+print(contrasena)
